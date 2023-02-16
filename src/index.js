62e0652e83cfd410e800d805b8a8bc46dbe15bca
Lucy Lee <leelucy412@gmail.com>
2023-02-16 17:31:16 -0500

    Setup initial mount for React app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
+import App from './components/App';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        {/* <Route path=":id" element={<Task />} />
+        <Route path="/status/:status" element={<Status />} />
+        <Route path="/priority/:priority" element={<Priority />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
